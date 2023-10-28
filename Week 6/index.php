945f4ebb7d1dc6e2e3c5b75456578fb41fac252f
Alex Glen <2455717@dundee.ac.uk>
2023-10-28 10:58:39 +0100

    uploaded template php code

---
+++ Week 6/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+    // PHP code goes here
+    echo "Hello World <br>";
+
+    // Declare a variable – use YOUR name
+    $name = "John Smith";
+    echo "My name is " . $name . "<br>";
+?>
+</body>
+</html>
